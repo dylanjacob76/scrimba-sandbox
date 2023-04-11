4ba238f0a041a4d94646e072c8cf38f95d1be5c9
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-04-11 15:16:07 -0400

    imperative code vs declarative code

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// ReactDOM.render(<h1>Hello, React!</h1>, document.getElementById("root"))
+// Declarative Code
+ReactDOM.render(<h1 className="header">Hello, React!</h1>, document.getElementById("root"));
 
-/* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
-
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
-*/
-
-let root = document.getElementById("root");
-let h1El = document.createElement("h1");
+// Imperative code
+const root = document.getElementById("root");
+const h1El = document.createElement("h1");
 
 h1El.textContent = "Hello React World!";
-h1El.classList.add("header");
+h1El.className = "header";
 
 root.appendChild(h1El);
